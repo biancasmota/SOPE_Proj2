0f093f776cd582796661dd0fcdd86d5010c64a50
carolina <carolinarosemback@gmail.com>
2020-05-01 20:35:16 +0100

    Needs Merge

--- U.c
+++ U.c
@@ -1,22 +1,7 @@
 #include "utils.h"
 #include "utils.c"
+#include "U.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <fcntl.h>
-#include <time.h>
-#include <unistd.h>
-
-#define MAX_THR 100000
-#define SIZE 256
-#define RAND 30
-
-
-int i = 1;
 
 void *pedidos(void *arg)
 {
---
+++ U.h
@@ -0,0 +1,22 @@
+#ifndef SOPE_PROJ2_U_H
+#define SOPE_PROJ2_U_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_THR 100000
+#define SIZE 256
+#define RAND 30
+
+int i = 1;
+
+
+#endif //SOPE_PROJ2_U_H
