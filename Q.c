07ddfb153cdc871490ca53a3bdad3aaca6089def
Iohan Sardinha <ioxsardinha@gmail.com>
2020-05-16 21:11:33 +0100

    semaphore

--- Q.c
+++ Q.c
@@ -165,6 +165,7 @@ void* process_client(void* arg)
             places[args->pl] = 0;
         }
     }
+    sem_post(&semaphore);
     free(args);
     close(fd);
     return NULL;
@@ -178,6 +179,8 @@ void* look_for_clients(void* arg)
     char  str[100000];
     mkfifo(this_args->FIFO_path,0660);
 
+
+
     if((main_fifo_fd=open((char*)this_args->FIFO_path,O_RDONLY)) < 0)
     {
         fprintf(stderr, "Error public opening FIFO\n");
@@ -189,6 +192,8 @@ void* look_for_clients(void* arg)
     {
         places[i] = 0;
     }
+
+    sem_init(&semaphore, 0, this_args->nthreads-1); 
 
     while(readline(main_fifo_fd,str))
     {
@@ -204,6 +209,7 @@ void* look_for_clients(void* arg)
                 fprintf(stderr,"System max threads reached\n");
                 break;
             }
+            sem_wait(&semaphore);
             curr_thread++;
             if(curr_thread > MAX_THREADS)
             {
--- Q.h
+++ Q.h
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define MAX_FILE_NAME_LENGHT 1024
 #define MAX_THREADS 100000
@@ -22,6 +23,8 @@ int* places;
 int line[MAX_THREADS] = {0};
 
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+sem_t semaphore; 
 
 typedef struct process_client_args
 {
